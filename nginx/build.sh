9f6b4edf98165ac53a1c1e09e09f5a01df9edcb6
Wilson Neto Dev <contato@wilsonneto.com.br>
2021-04-27 14:19:26 -0300

    adjusting build scripts

---
+++ nginx/build.sh
@@ -0,0 +1,3 @@
+docker build -t wilsonnetodev/pfa01proxy:v1 .
+docker tag wilsonnetodev/pfa01proxy:v1 wilsonnetodev/pfa01proxy:latest
+docker push wilsonnetodev/pfa01proxy:latest
--- up.sh
+++ up.sh
@@ -9,12 +9,12 @@ network_name=backend_network
 docker network create $network_name
 
 # running the database
-docker run --network=$network_name --name $db_hostname -e MYSQL_ROOT_PASSWORD=$db_password -p 3306:3306 -d mysql:latest
+docker run --network=$network_name --name $db_hostname -e MYSQL_ROOT_PASSWORD=$db_password -d mysql:latest
 
 # loop just for wait mysql be ready to receive connections
 until docker exec -i databasecontainer mysql -uroot -pbb91313065a0eba5f3ed1ea9d4ff74f6 -e "show databases"
 do
-  echo "this loop will try again until mysql container be ready..."
+  echo "this loop will try again until mysql container be ready... It can take 1 or 2 minutes"
   echo "waiting 20 seconds to try again..."
   sleep 20
 done
@@ -23,10 +23,10 @@ echo "seeding the database"
 docker exec -i $db_hostname mysql -uroot -p$db_password < ./mysql/data.sql
 
 echo "runnnig the api container"
-docker run --network=$network_name -d --name api -e SERVER_PORT=$api_port -e DB_HOST=$db_hostname -e DB_USER=$db_user -e DB_NAME=$db_name -e DB_SECRET=$db_password wilsonnetodev/apimodules:dev 
+docker run --network=$network_name -d --name api -e SERVER_PORT=$api_port -e DB_HOST=$db_hostname -e DB_USER=$db_user -e DB_NAME=$db_name -e DB_SECRET=$db_password wilsonnetodev/pfa01apimodules
 
 echo "runnnig the spa container"
-docker run --network=$network_name -d --name spa wilsonnetodev/spamodules:dev 
+docker run --network=$network_name -d --name spa wilsonnetodev/pfa01spamodules
 
-echo "runnnig the reverse proxy (nginx container)"
-docker run --network=$network_name -d --name proxy -p 8082:80 nginxtest
+echo "runnnig the reverse proxy"
+docker run --network=$network_name -d --name proxy -p 8080:80 wilsonnetodev/pfa01proxy
